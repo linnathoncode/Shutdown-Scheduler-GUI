b0cb1024d795f7f68b2d235713f00c2af6d077b8
Furkan Göçük <112954805+linnathoncode@users.noreply.github.com>
2024-06-22 13:41:13 +0300

    brightness change and new icon

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import os
+import subprocess
 
 
 def submit(hour, minute):
@@ -8,8 +9,14 @@ def submit(hour, minute):
         return -1
     else:
         os.system(f"shutdown /s /t {time}")
+        set_brightness(0)
         return 1
     
 def cancel():
     os.system("shutdown /a")
+    set_brightness(50)
+
+def set_brightness(brightness):
+    cmd = f"(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,{brightness})"
+    subprocess.run(["powershell", "-Command", cmd], capture_output=True)
 
